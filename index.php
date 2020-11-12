80366fb2453e3bcdd69e01fe22f668ab8ee07dfc
pyonpyon7654 <mahjiaqi@hotmail.com>
2020-11-13 04:20:45 +0800

    update bootstrap, login interface

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-
 <?php
 
   session_start();
@@ -24,46 +23,75 @@
 
 <html lang="en">
   <head>
-    <title> Learning Board </title>
+    <title>Learning Board</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, inital-scale=1">
     <!-- BootStrap include: this will include all needed files for BootStrap 4.3.1 -->
-    <?php include "css/BootStp.php" ?>
-    <!-- Below is the old css that does not use BootStrap -->
-    <!--<link rel="stylesheet" type="text/css" href="css/login.css">-->
+    <?php include "src/css/style.php" ?>
   </head>
-  <body>
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">System & Grading System</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+      </button>
 
+      <nav class="collapse navbar-collapse" id="navbarSupportedContent">
+           <!-- <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                  <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="#">Link</a>
+              </li>
+              <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                  <nav class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <a class="dropdown-item" href="#">Action</a>
+                      <a class="dropdown-item" href="#">Another action</a>
+                      <nav class="dropdown-divider"></nav>
+                      <a class="dropdown-item" href="#">Something else here</a>
+                  </nav>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link disabled" href="#">Disabled</a>
+              </li>
+          </ul> -->
+      </nav>
+  </nav>
+  </header>
+  <body class="bg-dark">
     <form action="loginProcess.php" method="post">
 
-      <div class="container">
-        <div>
-          <label for="loginUser"><b>Username</b></label>
-          <input type="email" placeholder="Enter email address" name="loginUser" required>
+      <div class="container-fluid">
+        <div class="row justify-content-center">
+        <div class="col-xl-3 jumbotron bg-white">
+          <div class="form-group">
+            <label for="loginUser"><b>Username</b></label>
+            <input type="email" class="form-control" placeholder="Enter email address" name="loginUser" required>
+          </div>
+
+          <div class="form-group">
+            <label for="password"><b>Password</b></label>
+            <input type="password" class="form-control" placeholder="Enter Password" name="password" required>
+          </div>
+
+          <div class="form-group">
+          <button type="submit" class="btnSubmit btn btn-danger btn-block">Login</button>
+          </div>
+          
+          <label>
+            <input type="checkbox" checked="checked" name="remember"> Remember me
+          </label>
+
+          <?php
+            if(isset($_SESSION["errorMsg"])) {
+              echo "<h3>" . $_SESSION['errorMsg'] . "</h3>";
+            }
+          ?>
         </div>
-
-        <div>
-          <label for="password"><b>Password</b></label>
-          <input type="password" placeholder="Enter Password" name="password" required>
         </div>
-
-        <div>
-        <button type="submit">Login</button>
-        </div>
-        
-        <label>
-          <input type="checkbox" checked="checked" name="remember"> Remember me
-        </label>
-
-        <?php
-          if(isset($_SESSION["errorMsg"])) {
-            echo "<h3>" . $_SESSION['errorMsg'] . "</h3>";
-          }
-        ?>
-
       </div>
-
     </form>
   </body>
-
 </html>
---
+++ src/css/style.php
@@ -0,0 +1,10 @@
+<?php ?>
+<!-- custom stylesheet -->
+<link rel="stylesheet" type="text/css" href="./src/custom-css/style.css">
+
+<!-- bootstrap-4.3.1 -->
+<link rel="stylesheet" type="text/css" href="./src/bootstrap-4.3.1/bootstrap.min.css">
+<script src="./bootstrap-4.3.1/bootstrap.min.js"></script>
+
+<!-- jquery-3.5.1 -->
+<script src="./bootstrap-4.3.1/jquery-3.5.1.min.js"></script>
