20ba42ef63cfac828138b4f9e39463b213ccc449
weirenye <57995805+weirenye@users.noreply.github.com>
2020-11-13 23:01:38 +0800

    Update _dbconn.php
    update to new db connection

--- _dbconn.php
+++ _dbconn.php
@@ -1,9 +1,9 @@
 <?php
 
-define("DBHOST", "sql12.freemysqlhosting.net");
-define("DBNAME", "sql12374841");
-define("DBUSER", "sql12374841");
-define("DBPASS", "GBpfzhtYQ5");
+define("DBHOST", "rm-gs595dd89hu8175hl6o.mysql.singapore.rds.aliyuncs.com");
+define("DBNAME", "sql1902686twr");
+define("DBUSER", "ict1902686twr");
+define("DBPASS", "RWT6862091");
 
 $conn = new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
 if(!$conn) {
