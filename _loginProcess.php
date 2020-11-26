47473d05c6f99ff41af47d551dd61fdc551ef5eb
pyonpyon7654 <mahjiaqi@hotmail.com>
2020-11-26 14:16:05 +0800

    stud

--- _loginProcess.php
+++ _loginProcess.php
@@ -75,7 +75,7 @@ if ($success == true) {
       }
 
       else if ($_SESSION['sessionToken']->getUserType() == 2) {
-        header('Location:Dashboard.php');
+        header('Location:studDashboard.php');
       }
     }
 
---
+++ studDashboard.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+  <?php include "interface/head/head.php" ?>
+  <?php include "interface/header/header.php" ?>
+
+  <body class="bg-dark">
+      <div class="container-fluid">
+        <!-- student nav bar here -->
+        <?php include "interface/header/stud_nav.php" ?>
+
+        <div class="row justify-content-center">
+        <div class="col-xl-8 jumbotron bg-white">
+          <!-- header -->
+          <div class="d-flex justify-content-between p-3">
+            <h5>Sky City</h5>
+          </div>
+          
+         
+
+          </div>
+        </div>
+      </div>
+  </body>
+</html>
